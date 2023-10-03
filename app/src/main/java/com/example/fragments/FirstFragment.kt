95fdf309af614e6e2b857566816684e7097ee5d6
claralv <lagunas.informatica@gmail.com>
2023-10-03 22:33:55 +0200

    comentario añadido

--- app/src/main/java/com/example/fragments/FirstFragment.kt
+++ app/src/main/java/com/example/fragments/FirstFragment.kt
@@ -22,7 +22,7 @@ class FirstFragment : Fragment() {
             param2 = it.getString(ARG_PARAM2)
         }
     }
-
+    //comentario
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
